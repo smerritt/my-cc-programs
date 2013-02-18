0c34c3f53e5ffdb6edd4bf47b2c1c5b0aec5e3d3
Sam Merritt <spam@andcheese.org>
2013-02-18 14:18:22 -0800

    Make clearup less fugly.
    You know, parameters instead of globals, sane function semantics,
    that sort of thing.

--- clearup.lua
+++ clearup.lua
@@ -12,17 +12,22 @@ local args = { ... }
 --   exit()
 -- end
 
-rows = tonumber(args[1])
-rowlen = tonumber(args[2])
-height = tonumber(args[3])
+box_width = tonumber(args[1])
+box_depth = tonumber(args[2])
+box_height = tonumber(args[3])
 
 function debug(text)
   -- comment out to silence
   print(text)
 end
 
-function eat_up()
-  for i=1,height-2 do
+-- ensures clear air for N blocks
+-- above the turtle
+function eat_up(how_far)
+  if how_far <= 0 then
+    return
+  end
+  for i=1,how_far-1 do
     while turtle.detectUp() do
       turtle.digUp()
       sleep(0.1)
@@ -33,33 +38,33 @@ function eat_up()
     turtle.digUp()
     sleep(0.1)
   end
-  for i=1,height-2 do
+  for i=1,how_far-1 do
     turtle.down()
   end
 end
 
-function eat_row()
-  for i=1,rowlen-1 do
+function eat_row(depth, height)
+  for i=1,depth do
     turtle.dig()
     turtle.forward()
-    eat_up()
+    eat_up(height-1)
   end
-  for i=1,rowlen-1 do
+  for i=1,depth do
     turtle.back()
   end
 end
 
-function eat_grid()
+function eat_room(width, depth, height)
   turtle.dig()
   turtle.forward()
-  eat_up()
-  for i=1,rows-1 do
-    eat_row()
+  eat_up(height-1)
+  for i=1,width do
+    eat_row(depth-1, height)
     turtle.turnRight()
     turtle.dig()
     turtle.forward()
     turtle.turnLeft()
-    eat_up()
+    eat_up(height-1)
   end
   eat_row()
 
@@ -71,11 +76,11 @@ function eat_grid()
   turtle.down()
 
   turtle.turnLeft()
-  for i=1,rows-1 do
+  for i=1,width-1 do
     turtle.forward()
   end
   turtle.turnRight()
   turtle.back()
 end
 
-eat_grid()
+eat_room(box_width, box_depth, box_height)
