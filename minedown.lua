24b16f669a8d47dbad872a1f3de1532416deb1de
Sam Merritt <spam@andcheese.org>
2013-06-29 11:48:18 -0700

    program to mine straight down

---
+++ minedown.lua
@@ -0,0 +1,37 @@
+-- mines straight down, comes back up
+
+loadfile("util")()
+
+bucket_slot = 1
+
+function debug(msg)
+  print(msg)
+end
+
+function munch()
+  util.ingestDown(bucket_slot)
+
+  wentDown = turtle.down()
+  if not wentDown then
+    -- probably bedrock
+    print("done; returning to surface")
+    return
+  end
+
+  -- as grows the stack, so goes the turtle
+  munch()
+  wentUp = turtle.up()
+  while not wentUp do
+    print("Failed to go up; retrying")
+    sleep(0.1)
+    turtle.digUp()   -- in case of gravel
+    wentUp = turtle.up()
+  end
+end
+
+if turtle.getFuelLevel() < 150 then
+  print(string.format("Not enough fuel: %d < 150", turtle.getFuelLevel()))
+  return
+else
+  munch()
+end
