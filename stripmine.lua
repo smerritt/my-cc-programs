8890267113eef21351e7012cc137f39160445042
Sam Merritt <spam@andcheese.org>
2013-07-04 21:19:16 -0700

    Add unloading to stripminer. Still need to do refueling.

--- stripmine.lua
+++ stripmine.lua
@@ -1,14 +1,32 @@
 -- mine in a strip N long
--- TODO: unloading into ender chest
+-- TODO: refueling
 
 local args = { ... }
-
 depth = tonumber(args[1])
 
 loadfile("util")()
 
+-- bucket slot is 1 in mineplus, so we can't use that
+unload_chest_slot = 2
+first_item_slot = 3
+
 function unload()
-  print("WRITE ME")
+  turtle.digUp()  -- just in case
+  turtle.select(unload_chest_slot)
+  if not turtle.placeUp() then
+    error("Failed placing ender chest above me")
+  end
+
+  for slot=first_item_slot,16 do
+    util.unloadUp(slot)
+  end
+
+  turtle.select(unload_chest_slot)
+  if not turtle.digUp() then
+    error("Failed to retrieve ender chest above me")
+  end
+
+  -- here's where the refueling would happen
 end
 
 for i=1,depth-1 do
